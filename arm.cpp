1521bebf48940ed6c5663f38c38d9b85a5abb1e2
casse <casse@irit.fr>
2018-07-13 16:29:19 +0200

    Adaptation to the new OTAWA version.

--- arm.cpp
+++ arm.cpp
@@ -220,7 +220,7 @@ static hard::PlainBank gpr("GPR", hard::Register::INT, 32, "r%d", 16);
 static hard::Register sr("sr", hard::Register::BITS, 32);
 static hard::MeltedBank misc("misc", &sr, (void *) 0);
 static const hard::RegBank *banks_tab[] = { &gpr, &misc };
-static genstruct::Table<const hard::RegBank *> banks_table(banks_tab, 2);
+static Array<const hard::RegBank *> banks_table(2, banks_tab);
 
 // register decoding
 class RegisterDecoder {
